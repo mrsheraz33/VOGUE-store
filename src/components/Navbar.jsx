155e9ec378a3c6044adebfad9931ccd64b8d7e5e
Muhammad Sheraz <mrsheraz325@gmail.com>
2026-08-10 23:56:48 +0500

    update files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-5 sm:px-8">
         <div className="flex items-center justify-between h-16 lg:h-20">
           <a href="/" className="lg:mx-0 mx-auto flex items-center gap-3">
-            <img src="/logo.png" alt="VOGUE" className="h-30 md:h-35 w-auto object-contain" />
+            <img src="/logo.png" alt="VOGUE" className="h-35 md:h-40 w-auto object-contain" />
           </a>
 
 
